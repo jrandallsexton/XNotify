b62605ff542f4d0b3bffbc648064475023af3f11
Randall Sexton <jrandallsexton@gmail.com>
2015-04-30 15:40:39 -0500

    Skeleton of interfaces

---
+++ XNotify.Tests/Fakes/FakeNotifiableEventProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using XNotify.Contracts;
+
+namespace XNotify.Tests.Fakes
+{
+    public class FakeNotifiableEventProvider : INotifiableEventProvider
+    {
+    }
+}
---
+++ XNotify/Providers/EmailNotificationProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XNotify.Providers
+{
+    class EmailNotificationProvider
+    {
+    }
+}
